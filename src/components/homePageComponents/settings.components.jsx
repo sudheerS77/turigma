eced1afb07de9e9e1a17cdc97682be0ed21e94e1
gsksudheer <sudheer.gsk77@gmail.com>
2022-09-13 21:40:17 +0530

    update colors

--- src/components/homePageComponents/settings.components.jsx
+++ src/components/homePageComponents/settings.components.jsx
@@ -8,7 +8,7 @@ const SettingsComponent = () => {
     const [state, setState] = useState({ x: 10, y: 10 });
   return (
     <>
-      <div className="flex flex-col items-start gap-2 mt-5 shadow-xl px-3 py-2 border border-gray-100">
+      <div className="flex flex-col items-start gap-8 mt-5 shadow-xl px-3 py-2 border border-gray-100">
         <h4>Settings</h4>
         <span className="w-1/2">
           <Slider
@@ -27,11 +27,11 @@ const SettingsComponent = () => {
           />
           {state.x}
         </span>
-        <div className="flex items-center gap-5 text-xl font-semibold">
+        <div className="flex items-center gap-5 text-xl font-semibold border border-gray-100 w-full p-2 rounded-md shadow-md">
           Select Warehouse
           <DropDown />
         </div>
-        <div className="flex flex-col items-start justify-center">
+        <div className="flex flex-col items-start justify-center w-full p-2 rounded-md shadow-md">
           <div className="flex items-center justify-center gap-2">
             <h4 className="text-sm font-semibold bg-dark-blue text-gray-50 px-2 py-1 rounded-md">
               Forecast Tolerage (in %) - [confidence interval]
@@ -90,7 +90,7 @@ const SettingsComponent = () => {
           Warehouses
           <DropDown />
         </div> */}
-        <div className="w-full">
+        <div className="w-full w-full p-2 rounded-md shadow-lg">
           <TableComponent />
         </div>
       </div>
